bcd12b442afe6f6a85306f4c533f792ddb0e3d63
PatrykJarzynka <patryk.jarzynka1234@gmail.com>
2022-06-14 11:13:42 +0200

    Prawie działa

---
+++ Backend/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const port = process.env.PORT;
+const uriDb = process.env.URI_DB;
+
+const connection = mongoose.connect(uriDb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
+connection
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(port, () => {
+      console.log("Server is running!");
+    });
+  })
+  .catch(() => {
+    console.log("Connection error!");
+    process.exit(1);
+  });
