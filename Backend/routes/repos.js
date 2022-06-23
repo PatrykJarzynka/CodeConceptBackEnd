82f624b3bf0286af64f92c1f9d4eda6ecb83ab56
PatrykJarzynka <patryk.jarzynka1234@gmail.com>
2022-06-23 13:58:42 +0200

    Dodano endpoint obsługujący patchowanie

--- Backend/routes/repos.js
+++ Backend/routes/repos.js
@@ -15,17 +15,44 @@ router.post("/sendData", async (req, res, next) => {
 
 router.get("/", async (req, res, next) => {
   const repos = await axios.get(
-    // "https://62b3525fa36f3a973d202c8f.mockapi.io/repositories"
-    "https://api.github.com/orgs/alibaba/repos?per_page=200"
+    "https://62b3525fa36f3a973d202c8f.mockapi.io/repositories"
+    // "https://api.github.com/orgs/alibaba/repos?per_page=200"
   );
 
   const reposData = repos.data;
   const reposNames = reposData.map((repo) => repo.name);
 
   const data = await service.getData();
-  const reposAndUsers = { data, reposNames }
-  
+  const reposAndUsers = { data, reposNames };
+
   res.json(reposAndUsers);
-})
+});
+
+router.get("/getData", async (req, res, next) => {
+  const data = await service.getData();
+  res.json(data);
+});
+
+router.patch("/patchData", async (req, res, next) => {
+  const id = req.body._id;
+  const { money } = req.body;
+  try {
+    if (money === undefined) {
+      res.status(400).json({ message: "missing field money" });
+    } else {
+      const result = await service.updateData(id, { money });
+      if (!result) {
+        res.status(404).json({ message: "Not Found" });
+      } else {
+        res.json({
+          message: `data patched to money = ${money}`,
+          data: result,
+        });
+      }
+    }
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
--- Backend/service/index.js
+++ Backend/service/index.js
@@ -3,7 +3,19 @@ const Data = require("./schemas/userData");
 const addData = ({ email, money, title }) =>
   Data.create({ email, money, title });
 
-const getData = () =>
-  Data.find({}).lean();
+const getData = () => Data.find({}).lean();
 
-module.exports = { addData, getData };
+const updateData = (id, money) =>
+  Data.findOneAndUpdate(
+    {
+      _id: id,
+    },
+    { $set: money },
+    {
+      new: true,
+      runValidators: true,
+      strict: "throw",
+    }
+  );
+
+module.exports = { addData, getData, updateData };
